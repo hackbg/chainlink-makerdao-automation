7c13fa7f54b5f72789ae62429e1e104f5c89d2ea
imollov <ivomollov@gmail.com>
2022-03-02 15:20:59 +0200

    feat: integrate DssVestTopUp in DssCronKeeper

--- test/DssCronKeeper.test.ts
+++ test/DssCronKeeper.test.ts
@@ -1,14 +1,25 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { DssCronKeeper, SampleJob, Sequencer } from "../typechain";
+import {
+  DssCronKeeper,
+  DssVestTopUpMock,
+  SampleJob,
+  Sequencer,
+} from "../typechain";
 
 const { HashZero } = ethers.constants;
 const { formatBytes32String } = ethers.utils;
+
+const ABI = ["function performJob()", "function performTopUp()"];
+const iface = new ethers.utils.Interface(ABI);
+const performJobEncoded = iface.encodeFunctionData("performJob");
+const performTopUpEndcoded = iface.encodeFunctionData("performTopUp");
 
 describe("DssCronKeeper", function () {
   let keeper: DssCronKeeper;
   let sequencer: Sequencer;
   let job: SampleJob;
+  let topUpMock: DssVestTopUpMock;
 
   beforeEach(async function () {
     const Sequencer = await ethers.getContractFactory("Sequencer");
@@ -18,7 +29,6 @@ describe("DssCronKeeper", function () {
 
     const SampleJob = await ethers.getContractFactory("SampleJob");
     job = await SampleJob.deploy(sequencer.address, 100);
-
     await sequencer.addJob(job.address);
 
     const DssCronKeeper = await ethers.getContractFactory("DssCronKeeper");
@@ -26,16 +36,34 @@ describe("DssCronKeeper", function () {
       sequencer.address,
       formatBytes32String("test")
     );
+
+    const DssVestTopUpMock = await ethers.getContractFactory(
+      "DssVestTopUpMock"
+    );
+    topUpMock = await DssVestTopUpMock.deploy();
+    await keeper.setTopUp(topUpMock.address);
   });
 
   describe("checkUpkeep", function () {
-    it("should return true if there are pending jobs", async function () {
-      const [upkeepNeeded] = await keeper.callStatic.checkUpkeep(HashZero);
+    it("should perform job if there are pending jobs", async function () {
+      const [upkeepNeeded, perfromData] = await keeper.callStatic.checkUpkeep(
+        HashZero
+      );
       expect(upkeepNeeded).to.eq(true);
+      expect(perfromData).to.eq(performJobEncoded);
     });
 
-    it("should return false if no pending jobs", async function () {
-      await keeper.performUpkeep(HashZero);
+    it("should perform top up if checker returns true", async function () {
+      await topUpMock.setChecker(true);
+      const [upkeepNeeded, perfromData] = await keeper.callStatic.checkUpkeep(
+        HashZero
+      );
+      expect(upkeepNeeded).to.eq(true);
+      expect(perfromData).to.eq(performTopUpEndcoded);
+    });
+
+    it("should return false if no pending jobs and no need for topUp", async function () {
+      await keeper.performUpkeep(performJobEncoded);
       const [upkeepNeeded] = await keeper.callStatic.checkUpkeep(HashZero);
       expect(upkeepNeeded).to.eq(false);
     });
@@ -43,16 +71,25 @@ describe("DssCronKeeper", function () {
 
   describe("performUpkeep", function () {
     it("should execute pending job", async function () {
-      await keeper.performUpkeep(HashZero);
+      await keeper.performUpkeep(performJobEncoded);
       expect(await job.last()).to.be.gt(0);
     });
 
     it("should not execute not pending job", async function () {
-      await keeper.performUpkeep(HashZero);
+      await keeper.performUpkeep(performJobEncoded);
       const last = await job.last();
 
-      await keeper.performUpkeep(HashZero);
+      await keeper.performUpkeep(performJobEncoded);
       expect(await job.last()).to.eq(last);
+    });
+
+    it("should execute top up", async function () {
+      const performTx = await keeper.performUpkeep(performTopUpEndcoded);
+      const performRc = await performTx.wait();
+      const topUpEvent = performRc.events?.find(
+        (e) => e.address === topUpMock.address
+      );
+      expect(topUpEvent).to.not.equal(undefined);
     });
   });
 });

c23d9577d55aa9296027b1789d4c6bb292f4545d
imollov <ivomollov@gmail.com>
2023-01-27 17:20:37 +0200

    refactor: unify tests

--- test/e2e/main.test.ts
+++ test/e2e/main.test.ts
@@ -1,266 +1,187 @@
 import { expect } from "chai";
 import { ethers, network } from "hardhat";
-import { BigNumberish, ContractReceipt, Wallet } from "ethers";
-
-import {
-  registerUpkeep,
-  getRegistrySigners,
-  keeperRegistryPerformUpkeep,
-  KeeperRegistryParams,
-  setupChainlinkAutomation,
-} from "../utils/chainlink-automation";
-import { parseEventFromABI } from "../utils/events";
+import * as chainlink from "../utils/chainlink-automation";
 import { setupPool } from "../utils/uniswap";
-import {
-  createVest,
-  setupDaiToken,
-  setupDssVest,
-  setupPaymentAdapter,
-  setupSampleJob,
-  setupSequencer,
-  deployDaiJoinMock,
-} from "../utils/maker";
-
+import { parseEventFromABI } from "../utils/events";
+import { Wallet } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import {
-  DssVestMintable,
-  ERC20PresetMinterPauser,
-  LinkTokenMock,
-  Sequencer,
-  DssVestTopUp,
-  SampleJob,
-  KeeperRegistry20,
-  KeeperRegistrar20,
-  IUniswapV3Pool,
-  NetworkPaymentAdapter,
-} from "../../typechain";
+import { DssVestTopUp } from "../../typechain/DssVestTopUp";
+import { NetworkPaymentAdapter } from "../../typechain/NetworkPaymentAdapter";
+import { SampleJob } from "../../typechain/SampleJob";
+import { ERC20PresetMinterPauser } from "../../typechain/ERC20PresetMinterPauser";
+import { LinkTokenMock } from "../../typechain/LinkTokenMock";
+import { KeeperRegistry20 } from "../../typechain/KeeperRegistry20";
+import { KeeperRegistrar20 } from "../../typechain/KeeperRegistrar20";
 
 const { parseEther, parseBytes32String, formatBytes32String } = ethers.utils;
 
-const {
-  KEEPER_REGISTRY_LOGIC,
-  NONFUNGIBLE_POSITION_MANAGER,
-  STAGING_LINK_TOKEN,
-  STAGING_LINK_USD_PRICE_FEED,
-  STAGING_PAYMENT_USD_PRICE_FEED,
-  STAGING_SWAP_ROUTER,
-  STAGING_UNISWAP_V3_FACTORY,
-  VOW,
-} = process.env;
+const keeperRegistryLogicAddress = process.env.KEEPER_REGISTRY_LOGIC;
+const positionManagerAddress = process.env.NONFUNGIBLE_POSITION_MANAGER;
+const linkTokenAddress = process.env.STAGING_LINK_TOKEN;
+const linkUsdPriceFeedAddress = process.env.STAGING_LINK_USD_PRICE_FEED;
+const daiUsdPriceFeedAddress = process.env.STAGING_PAYMENT_USD_PRICE_FEED;
+const swapRouterAddress = process.env.STAGING_SWAP_ROUTER;
+const uniswapV3FactoryAddress = process.env.STAGING_UNISWAP_V3_FACTORY;
+const vowAddress = process.env.VOW;
 
-const DssCronKeeperParams = {
-  NETWORK_NAME: formatBytes32String("test"),
-};
-
-const SequencerParams = {
-  WINDOW: 1,
-};
-
-const SampleJobParams = {
-  MAX_DURATION: 100,
-};
-
-const DssVestTopUpParams = {
-  UNISWAP_POOL_FEE: 3000,
-  SLIPPAGE_TOLERANCE: 2,
-};
-
-const PaymentAdapterParams = {
-  MIN_PAYMENT: parseEther("1"),
-  BUFFER_MAX: parseEther("1"),
-};
-
-const PoolParams = {
-  LINK_TOKEN_LIQUIDITY: parseEther("100"),
-  DAI_TOKEN_LIQUIDITY: parseEther("100"),
-};
-
-const DaiTokenParams = {
-  DEFAULT_ACCOUNT_BALANCE: parseEther("100"),
-};
-
-const DssVestParams = {
-  CAP: parseEther("100"),
-};
-
-const VestPlanParams = {
-  ID: 1,
-  TOTAL_AMOUNT: parseEther("100"),
-  DURATION_IN_SEC: 1,
-  CLIFF_PERIOD_IN_SEC: 0,
-};
-
-const UpkeepParams = {
-  NAME: "test123",
-  ADMIN_EMAIL: "test@example.com",
-  GAS_LIMIT: 500000,
-  INITIAL_FUNDING: parseEther("1"),
-  CHECK_DATA: "0x",
-  OFFCHAIN_CONFIG: "0x",
-};
-
-async function setupDssCronKeeper(sequencer: Sequencer, networkName: string) {
-  const DssCronKeeper = await ethers.getContractFactory("DssCronKeeper");
-  const cronKeeper = await DssCronKeeper.deploy(sequencer.address, networkName);
-  await sequencer.addNetwork(networkName);
-  return cronKeeper;
-}
-
-async function deployDssVestTopup(
-  upkeepId: BigNumberish,
-  registryAddress: string,
-  daiTokenAddress: string,
-  linkTokenAddress: string,
-  paymentUsdPriceFeedAddress: string,
-  linkUsdPriceFeedAddress: string,
-  swapRouterAddress: string,
-  uniswapPoolFee: BigNumberish,
-  slippageTolerance: BigNumberish
+if (
+  !keeperRegistryLogicAddress ||
+  !positionManagerAddress ||
+  !linkTokenAddress ||
+  !linkUsdPriceFeedAddress ||
+  !daiUsdPriceFeedAddress ||
+  !swapRouterAddress ||
+  !uniswapV3FactoryAddress ||
+  !vowAddress
 ) {
-  const DssVestTopUp = await ethers.getContractFactory("DssVestTopUp");
-  const topUp = await DssVestTopUp.deploy(
-    upkeepId,
-    registryAddress,
-    daiTokenAddress,
-    linkTokenAddress,
-    paymentUsdPriceFeedAddress,
-    linkUsdPriceFeedAddress,
-    swapRouterAddress,
-    uniswapPoolFee,
-    slippageTolerance
-  );
-  await topUp.deployed();
-  return topUp;
-}
-
-function parseSwapEvent(tx: ContractReceipt) {
-  const swappedDAIEventABI = [
-    "event SwappedDaiForLink(uint256 amountIn, uint256 amountOut)",
-  ];
-  return parseEventFromABI(tx, swappedDAIEventABI);
-}
-
-function parseTopUpEvent(tx: ContractReceipt) {
-  const topUpEventABI = [
-    "event TopUp(uint256 bufferSize, uint256 daiBalance, uint256 daiSent)",
-  ];
-  return parseEventFromABI(tx, topUpEventABI);
-}
-
-function getUpkeepPerformTotalSpent(tx: ContractReceipt) {
-  return tx.events?.find((e) => e.event === "UpkeepPerformed")?.args
-    ?.totalPayment;
+  throw new Error("Missing required env variable(s)!");
 }
 
 describe("E2E", function () {
-  let registry: KeeperRegistry20;
-  let registrar: KeeperRegistrar20;
-  let linkToken: LinkTokenMock;
-  let upkeepId: string;
-  let owner: SignerWithAddress;
-  let registrySigners: Wallet[];
-  let dssVest: DssVestMintable;
+  const vestingPlanDuration = 1;
+
   let topUp: DssVestTopUp;
   let paymentAdapter: NetworkPaymentAdapter;
-  let daiToken: ERC20PresetMinterPauser;
   let job: SampleJob;
-
-  if (
-    !STAGING_LINK_TOKEN ||
-    !STAGING_LINK_USD_PRICE_FEED ||
-    !STAGING_PAYMENT_USD_PRICE_FEED ||
-    !STAGING_SWAP_ROUTER ||
-    !VOW ||
-    !KEEPER_REGISTRY_LOGIC ||
-    !STAGING_UNISWAP_V3_FACTORY ||
-    !NONFUNGIBLE_POSITION_MANAGER
-  ) {
-    throw new Error("Missing required env variables!");
-  }
+  let upkeepId: string;
+  let daiToken: ERC20PresetMinterPauser;
+  let linkToken: LinkTokenMock;
+  let registry: KeeperRegistry20;
+  let registrar: KeeperRegistrar20;
+  let registrySigners: Wallet[];
+  let owner: SignerWithAddress;
 
   before(async function () {
     [owner] = await ethers.getSigners();
-    linkToken = await ethers.getContractAt("LinkTokenMock", STAGING_LINK_TOKEN);
-    registrySigners = getRegistrySigners();
+    linkToken = await ethers.getContractAt("LinkTokenMock", linkTokenAddress);
+    registrySigners = chainlink.getRegistrySigners();
   });
 
   beforeEach(async function () {
-    const sequencer = await setupSequencer(SequencerParams.WINDOW);
+    // setup dai token
+    const ERC20 = await ethers.getContractFactory("ERC20PresetMinterPauser");
+    daiToken = await ERC20.deploy("Test DAI", "DAI");
+    await daiToken.mint(owner.address, parseEther("100"));
 
-    daiToken = await setupDaiToken(
-      owner,
-      DaiTokenParams.DEFAULT_ACCOUNT_BALANCE
+    // setup sequencer
+    const Sequencer = await ethers.getContractFactory("Sequencer");
+    const sequencer = await Sequencer.deploy();
+    await sequencer.file(formatBytes32String("window"), 1);
+    const SampleJob = await ethers.getContractFactory("SampleJob");
+    job = await SampleJob.deploy(sequencer.address, 100);
+    await sequencer.addJob(job.address);
+
+    // setup vest
+    const DssVest = await ethers.getContractFactory("DssVestMintable");
+    const dssVest = await DssVest.deploy(daiToken.address);
+    await dssVest.file(formatBytes32String("cap"), parseEther("100"));
+    await daiToken.grantRole(
+      ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE")),
+      dssVest.address
     );
+    const DaiJoinMock = await ethers.getContractFactory("DaiJoinMock");
+    const daiJoin = await DaiJoinMock.deploy(daiToken.address);
 
-    job = await setupSampleJob(sequencer, SampleJobParams.MAX_DURATION);
-    dssVest = await setupDssVest(daiToken, DssVestParams.CAP);
-    const daiJoinMock = await deployDaiJoinMock(daiToken.address);
-
-    const cronKeeper = await setupDssCronKeeper(
-      sequencer,
-      DssCronKeeperParams.NETWORK_NAME
+    // setup cron keeper
+    const DssCronKeeper = await ethers.getContractFactory("DssCronKeeper");
+    const networkName = formatBytes32String("test");
+    const cronKeeper = await DssCronKeeper.deploy(
+      sequencer.address,
+      networkName
     );
+    await sequencer.addNetwork(networkName);
 
-    ({ registry, registrar } = await setupChainlinkAutomation(
+    // setup chainlink automation contracts
+    ({ registry, registrar } = await chainlink.setupChainlinkAutomation(
       owner,
       linkToken,
-      KEEPER_REGISTRY_LOGIC
+      keeperRegistryLogicAddress
     ));
-    upkeepId = await registerUpkeep(
+
+    // register cron keeper as upkeep
+    const upkeepName = "test123";
+    const upkeepAdminEmail = "test@example.com";
+    const upkeepGasLimit = 500000;
+    const upkeepInitialFunding = parseEther("1");
+    const upkeepCheckData = "0x";
+    const upkeepOffchainConfig = "0x";
+    upkeepId = await chainlink.registerUpkeep(
       cronKeeper.address,
       linkToken.address,
       registrar.address,
       owner.address,
-      UpkeepParams.ADMIN_EMAIL,
-      UpkeepParams.NAME,
-      UpkeepParams.GAS_LIMIT,
-      UpkeepParams.OFFCHAIN_CONFIG,
-      UpkeepParams.INITIAL_FUNDING,
-      UpkeepParams.CHECK_DATA
+      upkeepAdminEmail,
+      upkeepName,
+      upkeepGasLimit,
+      upkeepOffchainConfig,
+      upkeepInitialFunding,
+      upkeepCheckData
     );
 
-    topUp = await deployDssVestTopup(
+    // setup topup contract
+    const DssVestTopUp = await ethers.getContractFactory("DssVestTopUp");
+    const uniswapPoolFee = 3000;
+    const slippageTolerancePercent = 2;
+    topUp = await DssVestTopUp.deploy(
       upkeepId,
       registry.address,
       daiToken.address,
       linkToken.address,
-      STAGING_PAYMENT_USD_PRICE_FEED,
-      STAGING_LINK_USD_PRICE_FEED,
-      STAGING_SWAP_ROUTER,
-      DssVestTopUpParams.UNISWAP_POOL_FEE,
-      DssVestTopUpParams.SLIPPAGE_TOLERANCE
+      daiUsdPriceFeedAddress,
+      linkUsdPriceFeedAddress,
+      swapRouterAddress,
+      uniswapPoolFee,
+      slippageTolerancePercent
     );
 
-    paymentAdapter = await setupPaymentAdapter(
+    // setup network payment adapter
+    const NetworkPaymentAdapter = await ethers.getContractFactory(
+      "NetworkPaymentAdapter"
+    );
+    const vestingPlanId = 1;
+    const bufferMax = parseEther("1");
+    const minPayment = parseEther("1");
+    const treasury = topUp.address;
+    paymentAdapter = await NetworkPaymentAdapter.deploy(
       dssVest.address,
-      VestPlanParams.ID,
-      topUp.address,
-      daiJoinMock.address,
-      VOW,
-      PaymentAdapterParams.BUFFER_MAX,
-      PaymentAdapterParams.MIN_PAYMENT
+      vestingPlanId,
+      treasury,
+      daiJoin.address,
+      vowAddress
+    );
+    await paymentAdapter.file(formatBytes32String("bufferMax"), bufferMax);
+    await paymentAdapter.file(
+      formatBytes32String("minimumPayment"),
+      minPayment
     );
     await topUp.setPaymentAdapter(paymentAdapter.address);
 
-    await createVest(
-      dssVest,
+    // create vesting plan
+    const blockNum = await ethers.provider.getBlockNumber();
+    const block = await ethers.provider.getBlock(blockNum);
+    const vestingPlanStartTime = block.timestamp;
+    const vestingPlanAmount = parseEther("100");
+    const vestingPlanCliff = 0;
+    await dssVest.create(
       paymentAdapter.address,
-      VestPlanParams.TOTAL_AMOUNT,
-      VestPlanParams.DURATION_IN_SEC,
-      VestPlanParams.CLIFF_PERIOD_IN_SEC,
-      owner
+      vestingPlanAmount,
+      vestingPlanStartTime,
+      vestingPlanDuration,
+      vestingPlanCliff,
+      owner.address
     );
 
+    // enable upkeep auto refunding
     await cronKeeper.setUpkeepRefunder(topUp.address);
   });
 
   describe("Execute jobs", function () {
     it("should have executed job successfully", async function () {
-      await keeperRegistryPerformUpkeep(
+      await chainlink.keeperRegistryPerformUpkeep(
         registry,
         registrySigners,
         upkeepId,
-        KeeperRegistryParams.F
+        chainlink.KeeperRegistryParams.F
       );
 
       const [workable, reasonBytes] = await job.workable(
@@ -273,11 +194,11 @@ describe("E2E", function () {
 
     it("should reduce upkeep balance after job execution", async function () {
       const { balance: preBalance } = await registry.getUpkeep(upkeepId);
-      await keeperRegistryPerformUpkeep(
+      await chainlink.keeperRegistryPerformUpkeep(
         registry,
         registrySigners,
         upkeepId,
-        KeeperRegistryParams.F
+        chainlink.KeeperRegistryParams.F
       );
       const { balance: postBalance } = await registry.getUpkeep(upkeepId);
       expect(preBalance).to.be.gt(postBalance);
@@ -285,40 +206,27 @@ describe("E2E", function () {
   });
 
   describe("Refund upkeep", function () {
-    let pool: IUniswapV3Pool;
-
     beforeEach(async function () {
-      pool = await setupPool(
+      const poolLinkTokenLiquidity = parseEther("100");
+      const poolDaiTokenLiquidity = parseEther("100");
+      await setupPool(
         linkToken,
         daiToken,
         owner,
-        PoolParams.LINK_TOKEN_LIQUIDITY,
-        PoolParams.DAI_TOKEN_LIQUIDITY,
-        STAGING_UNISWAP_V3_FACTORY,
-        NONFUNGIBLE_POSITION_MANAGER
+        poolLinkTokenLiquidity,
+        poolDaiTokenLiquidity,
+        uniswapV3FactoryAddress,
+        positionManagerAddress
       );
-
-      await network.provider.send("evm_increaseTime", [
-        VestPlanParams.DURATION_IN_SEC,
-      ]);
+      // advance time to accrue all tokens in vesting plan
+      await network.provider.send("evm_increaseTime", [vestingPlanDuration]);
       await network.provider.send("evm_mine");
-
-      expect(
-        await pool.liquidity(),
-        "no liquidity in link/payment token pool"
-      ).to.be.gt(0);
-
-      expect(
-        await dssVest.unpaid(VestPlanParams.ID),
-        "no accrued tokens in vest"
-      ).to.equal(DssVestParams.CAP);
     });
 
     it("should refund upkeep when balance falls below threshold", async function () {
-      const { balance: initialBalance } = await registry
-        .connect(ethers.constants.AddressZero)
-        .callStatic.getUpkeep(upkeepId);
-
+      const { balance: initialBalance } = await registry.callStatic.getUpkeep(
+        upkeepId
+      );
       // increase threshold above balance so it has to refund upkeep
       await paymentAdapter.file(
         formatBytes32String("bufferMax"),
@@ -326,27 +234,37 @@ describe("E2E", function () {
       );
       expect(await topUp.shouldRefundUpkeep(), "refund not needed").to.eq(true);
 
-      const tx = await keeperRegistryPerformUpkeep(
+      // do the thing
+      const tx = await chainlink.keeperRegistryPerformUpkeep(
         registry,
         registrySigners,
         upkeepId,
-        KeeperRegistryParams.F
+        chainlink.KeeperRegistryParams.F
       );
 
-      const { balance } = await registry
-        .connect(ethers.constants.AddressZero)
-        .callStatic.getUpkeep(upkeepId);
+      const { balance } = await registry.callStatic.getUpkeep(upkeepId);
 
-      const { daiSent } = parseTopUpEvent(tx);
-      const { amountIn, amountOut } = parseSwapEvent(tx);
-      const performUpkeepLinkSpent = getUpkeepPerformTotalSpent(tx);
+      // get dai transferred from event params of payment adapter
+      const topUpEventABI = [
+        "event TopUp(uint256 bufferSize, uint256 daiBalance, uint256 daiSent)",
+      ];
+      const { daiSent } = parseEventFromABI(tx, topUpEventABI);
 
-      expect(daiSent, "dai received wasn't used").eq(amountIn);
+      // get swap event params from topup contract
+      const swappedDAIEventABI = [
+        "event SwappedDaiForLink(uint256 amountIn, uint256 amountOut)",
+      ];
+      const { amountIn, amountOut } = parseEventFromABI(tx, swappedDAIEventABI);
 
-      expect(
-        initialBalance.add(amountOut).sub(performUpkeepLinkSpent),
-        "swapped link amount not refunded to upkeep"
-      ).eq(balance);
+      // get total link spent from event
+      const performUpkeepLinkSpent = tx.events?.find(
+        (e) => e.event === "UpkeepPerformed"
+      )?.args?.totalPayment;
+
+      expect(daiSent).eq(amountIn);
+      expect(initialBalance.add(amountOut).sub(performUpkeepLinkSpent)).eq(
+        balance
+      );
     });
   });
 });

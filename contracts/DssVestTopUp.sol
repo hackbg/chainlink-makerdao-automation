c7a27f34993dff670134bce990a4396faa11b859
imollov <ivomollov@gmail.com>
2023-04-10 14:35:04 +0300

    refactor(topup): drop redundant import

--- contracts/DssVestTopUp.sol
+++ contracts/DssVestTopUp.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
